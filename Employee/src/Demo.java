1ea1df9c3f2c8519a1d0a4a5ed4043224b8645c7
laxmi.kurkute <laxmi.kurkute@N12924.intellectdesign.com>
2021-09-16 17:27:58 +0530

    laxmi

--- Employee/src/Demo.java
+++ Employee/src/Demo.java
@@ -1,7 +1,16 @@
 
 public class Demo {
+	
+	public static int multi() {
+		int a=10;
+		int b=2;
+		int c=a*b;
+		System.out.println("Multiplication is :"+c);
+		return c;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("hello");
+		multi();
 	}
 }
